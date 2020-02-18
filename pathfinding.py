6ab875f655a0bbac8ba257700977356e3774bd1b
Teardrop13 <michal.lewandowski99@gmail.com>
2020-02-18 12:57:12 +0100

    changing node modes

--- pathfinding.py
+++ pathfinding.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 import time
 
+# Node modes:
 # 0 - normal
 # 1 - obstacle
 # 2 - start
@@ -9,38 +10,69 @@ import time
 
 black = 0, 0, 0
 blue = 0, 0, 255
+green = 0, 255, 0 
+red = 255, 0, 0
+white = 255, 255, 255
 blockSize = 40
 width = 20
 height = 15
+margin = 1
 size = 800, 600
 
 pygame.init()
 screen = pygame.display.set_mode(size)
 
-class Field:
+class Node:
     def __init__(self, x, y):
-        self.type = 0
+        self.mode = 0
         self.x = x
         self.y = y
-        self.rect = pygame.Rect(x*blockSize, y*blockSize, blockSize, blockSize)
+        self.closed = False
+        # distance from start
+        self.g_cost = 0
+        # distance to target
+        self.h_cost = 0
+        # g_cost + h_cost
+        self.f_cost = 0
+        self.rect = pygame.Rect(x*blockSize + margin, y*blockSize + margin, blockSize - 2*margin, blockSize - 2*margin)
 
     def drawRect(self):
-        pygame.draw.rect(screen, blue, self.rect)
+        if self.mode == 0:
+            pygame.draw.rect(screen, white, self.rect)
+        if self.mode == 1:
+            pygame.draw.rect(screen, blue, self.rect)
+        if self.mode == 2:
+            pygame.draw.rect(screen, red, self.rect)
+        if self.mode == 3:
+            pygame.draw.rect(screen, green, self.rect)
+
+    def setMode(self, mode):
+        self.mode = mode
 
 
-array = []
-for i in range(width):
-    for k in range(height):
-        array.append(Field(i, k))
+array = [[]]
+for x in range(width):
+    array.append([])
+    for y in range(height):
+        array[x].append(Node(x, y))
 
 buttonEvents = []
 
 buttonEvents = pygame.key.get_pressed()
 
-for i in array:
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-    i.drawRect()
-    time.sleep(0.1)
+    for x in range(width):
+        array.append([])
+        for y in range(height):
+            array[x][y].drawRect()
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    x = int(mouse[0] / blockSize)
+    y = int(mouse[1] / blockSize)
+    if click[0] == 1:
+        array[x][y].setMode(1)
+    time.sleep(0.05)
     pygame.display.update()
